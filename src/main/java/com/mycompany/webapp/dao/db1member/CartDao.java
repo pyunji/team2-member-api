c5f84fdf5290cd67e596aa565b9a69f2cd99249c
HYUNJI PYUN <pyunjiology@gmail.com>
2021-12-07 20:12:45 +0900

    장바구니 기능 주문 폼으로 가는 것 빼고 잘됨

--- src/main/java/com/mycompany/webapp/dao/db1member/CartDao.java
+++ src/main/java/com/mycompany/webapp/dao/db1member/CartDao.java
@@ -3,10 +3,30 @@ package com.mycompany.webapp.dao.db1member;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.mycompany.webapp.dto.CartItem;
+import com.mycompany.webapp.dto.CartUpdate;
+import com.mycompany.webapp.dto.Color;
+import com.mycompany.webapp.dto.Size;
+import com.mycompany.webapp.vo.Cart;
 
 @Mapper
 public interface CartDao {
 	List<CartItem> selectPidList(String mid);
+	
+	List<Color> selectColorsByPcommonId(String pcommonId);
+	
+	List<Cart> selectBeforeUpdate(CartUpdate cartUpdate);
+	int update(CartUpdate cartUpdate);
+	int deleteToUpdate(CartUpdate cartUpdate);
+	int updateQuantity(CartUpdate cartUpdate);
+	
+	
+	
+	List<Size> selectSizesByPcolorId(String PcolorId);
+	int updateCountByQuantity(@Param("quantity") int quantity, @Param("pstockId") String pstockId, @Param("mid") String mid);
+	int updatePstockId(@Param("newPstockId") String newPstockId, @Param("mid") String mid, @Param("oldPstockId") String oldPstockId);
+	int insertCart(Cart cart);
+	int deleteByMidAndPstockid(Cart cart);
 }
--- src/main/java/com/mycompany/webapp/service/CartService.java
+++ src/main/java/com/mycompany/webapp/service/CartService.java
@@ -3,12 +3,18 @@ package com.mycompany.webapp.service;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
 import com.mycompany.webapp.dao.db1member.CartDao;
 import com.mycompany.webapp.dao.db2product.ProductDao;
 import com.mycompany.webapp.dto.CartItem;
+import com.mycompany.webapp.dto.CartUpdate;
+import com.mycompany.webapp.dto.Color;
+import com.mycompany.webapp.dto.ProductToCart;
+import com.mycompany.webapp.dto.Size;
+import com.mycompany.webapp.vo.Cart;
 
 @Service
 public class CartService {
@@ -17,23 +23,70 @@ public class CartService {
 
 	@Resource private ProductDao productDao;
 	
+	@Transactional
 	public List<CartItem> getList(String mid) {
 		List<CartItem> dataFromMemberDB = cartDao.selectPidList(mid);
-		for(CartItem cartItemFromMDB : dataFromMemberDB) {
-			String pstockid = cartItemFromMDB.getPstockid();
-			String pcommonid = cartItemFromMDB.getPcommonid();
-			String pcolorid = pcommonid + "_" + cartItemFromMDB.getOccode();
-			cartItemFromMDB.setPcolorid(pcolorid);
+		for(CartItem cartItem : dataFromMemberDB) {
+			String pstockid = cartItem.getPstockid();
+			String pcommonid = cartItem.getPcommonid();
+			String pcolorid = pcommonid + "_" + cartItem.getOccode();
+			cartItem.setPcolorid(pcolorid);
 			CartItem dataFromPDB1 = productDao.selectPnameAndBname(pcommonid, pcolorid, pstockid);
-			cartItemFromMDB.setBname(dataFromPDB1.getBname());
-			cartItemFromMDB.setPname(dataFromPDB1.getPname());
-			cartItemFromMDB.setImg1(dataFromPDB1.getImg1());
-			cartItemFromMDB.setPprice(dataFromPDB1.getPprice());
-			cartItemFromMDB.setStock(dataFromPDB1.getStock());
-			cartItemFromMDB.setProductColorData(productDao.selectProductColorStockData(pcommonid));
+			cartItem.setBname(dataFromPDB1.getBname());
+			cartItem.setPname(dataFromPDB1.getPname());
+			cartItem.setImg1(dataFromPDB1.getImg1());
+			cartItem.setPprice(dataFromPDB1.getPprice());
+			cartItem.setStock(dataFromPDB1.getStock());
+			cartItem.setProductColorData(productDao.selectProductColorStockData(pcommonid));
 		}
 		return dataFromMemberDB;
 		
 	}
+	
+	public List<Color> getColors(String pcommonId) {
+		return cartDao.selectColorsByPcommonId(pcommonId);
+	}
+	
+	public List<Size> getSizesByPcolorId(String pcolorId) {
+		return cartDao.selectSizesByPcolorId(pcolorId);
+	}
 
+	@Transactional
+	public int updateQuantity(int quantity, String pstockId, String mid ) {
+		return cartDao.updateCountByQuantity(quantity, pstockId, mid);
+	}
+	
+	@Transactional
+	public int updateOptions(ProductToCart product, String mid) {
+		String oldPstockId = product.getProductStockId();
+		String newPstockId = product.getProductCommonId() + "_" + product.getColorCode() + "_" + product.getSizeCode();
+		int quantity = product.getQuantity();
+		
+		CartUpdate cartUpdate = new CartUpdate();
+		cartUpdate.setMemberId(mid);
+		cartUpdate.setNewPstockId(newPstockId);
+		cartUpdate.setOldPstockId(oldPstockId);
+		cartUpdate.setNewQuantity(quantity);
+		
+		List<Cart> selectBeforeUpdate = cartDao.selectBeforeUpdate(cartUpdate);
+		if (selectBeforeUpdate.size() < 1) {
+			return cartDao.update(cartUpdate);
+		} else {
+			cartUpdate.setOldQuantity(selectBeforeUpdate.get(0).getQuantity());
+			cartDao.updateQuantity(cartUpdate);
+			return cartDao.deleteToUpdate(cartUpdate);
+		}
+	}
+
+	@Transactional
+	public int insertCart(Cart cart) {
+		return cartDao.insertCart(cart);
+	}
+
+	@Transactional
+	public void deleteCart(List<Cart> carts) {
+		for(Cart cart:carts) {
+			cartDao.deleteByMidAndPstockid(cart);
+		}
+	}
 }
