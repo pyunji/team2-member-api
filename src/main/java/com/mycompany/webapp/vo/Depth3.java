7aa700b4cfbcc809716ff9f6bc550988c1f697b0
HYUNJI PYUN <pyunjiology@gmail.com>
2021-12-07 11:59:33 +0900

    코드 리팩토링

--- src/main/java/com/mycompany/webapp/vo/Depth3.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.webapp.vo;
-
-import lombok.Data;
-
-@Data
-public class Depth3 {
-	private String name;
-}
--- src/main/java/com/mycompany/webapp/vo/ProductCategory.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.webapp.vo;
-
-import lombok.Data;
-
-@Data
-public class ProductCategory {
-	private String productCommonId;
-	private int categoryNo;
-}
