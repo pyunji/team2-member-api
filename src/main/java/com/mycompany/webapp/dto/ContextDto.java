bdfcd563c0d277d6d16b29f96b145a39bf6badd9
SeoHoehyeong <86947580+SeoHoeHyeong@users.noreply.github.com>
2021-12-22 20:21:56 +0900

    홈 화면 디스플레이 관리 추가

---
+++ src/main/java/com/mycompany/webapp/dto/ContextDto.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.webapp.dto;
+
+import lombok.Data;
+
+@Data
+public class ContextDto {
+	String context0;
+	String context1;
+	String context2;
+	
+	int index0;
+	int index1;
+	int index2;
+	
+}
